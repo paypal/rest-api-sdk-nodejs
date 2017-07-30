bca08ddb0d4b2ba70dcd2cea3f0dcf2ac8cd9f56
devconcept <devconcept@outlook.com>
2017-07-30 08:20:28 -0400

    More thorough tests

--- test/promises.js
+++ test/promises.js
@@ -7,7 +7,13 @@ var chai = require('chai'),
     Promise = global.Promise || require('es6-promise');
 
 var paypal = require('../');
-require('./configure');
+
+// Changing the client id is required so tokens from other tests are not reused
+paypal.configure({
+    'mode': 'sandbox',
+    'client_id': 'clientId1',
+    'client_secret': 'clientSecret'
+});
 
 
 describe('Promises', function () {
@@ -15,6 +21,7 @@ describe('Promises', function () {
     var payout_item_id2 = '5UD3FSLKEZ63C';
     var payout_item_id3 = 'UK6HNZEQX8M8S';
     var sender_batch_id = "t1000";
+
 
     var create_batch_payout_json = {
         "sender_batch_header": {
@@ -59,24 +66,51 @@ describe('Promises', function () {
         require('./mocks/promises');
     }
 
-    it('uses promise style request', function () {
-        return paypal.payout.create(create_batch_payout_json).then(function (payout) {
-            expect(payout.batch_header.payout_batch_id).to.not.equal(null);
-            expect(payout.batch_header.sender_batch_header.sender_batch_id).to.equal(sender_batch_id);
-            expect(payout.links).to.not.be.empty;
+    it('joins requests with different clientIds and refreshing tokens without failing', function () {
+        return paypal.payout.create(create_batch_payout_json) // no token for client, 1s expiration, 1.1s request delay
+            .then(function (payout) {
+                expect(payout).to.have.property('batch_header');
+                return Promise.all([
+                    paypal.payoutItem.get(payout_item_id1), // token expired
+                    paypal.payoutItem.get(payout_item_id2, { // different client, no token
+                        'client_id': 'clientId2',
+                        'client_secret': 'clientSecret2'
+                    }),
+                    paypal.payoutItem.get(payout_item_id3) // refreshing
+                ]);
+            })
+            .then(function (payoutItems) {
+                expect(payoutItems).to.have.length(3);
+                payoutItems.forEach(function (item) {
+                    expect(item).to.have.property('payout_item_id');
+                });
+            });
+    });
+
+    it('should refresh the token when concurrent calls receive a 401 response', function () {
+        return Promise.all([
+            paypal.payout.create(create_batch_payout_json), // valid token, receive 401, not refreshing
+            paypal.payoutItem.get(payout_item_id1) // valid token, receive 401, refreshing
+        ]).then(function (response) {
+            expect(response).to.have.length(2);
+            expect(response[0]).to.have.property('batch_header');
+            expect(response[1]).to.have.property('payout_item_id');
         });
     });
 
-    it('joins requests with different clientIds and refreshing tokens without failing', function () {
+    it('should queue the call when a response receives a 401 response', function () {
+        var delayedPromise = new Promise(function (resolve, reject) {
+            setTimeout(function () {
+                paypal.payoutItem.get(payout_item_id1).then(resolve, reject);
+            }, 100);
+        });
         return Promise.all([
-            paypal.payoutItem.get(payout_item_id1),
-            paypal.payoutItem.get(payout_item_id2, {
-                'client_id': 'client_id',
-                'client_secret': 'client_secret'
-            }),
-            paypal.payoutItem.get(payout_item_id3)
-        ]).then(function (payoutItems) {
-            expect(payoutItems).not.to.be.empty;
+            paypal.payout.create(create_batch_payout_json), // valid token, receive 401, not refreshing
+            delayedPromise // refreshing
+        ]).then(function (response) {
+            expect(response).to.have.length(2);
+            expect(response[0]).to.have.property('batch_header');
+            expect(response[1]).to.have.property('payout_item_id');
         });
     });
 
